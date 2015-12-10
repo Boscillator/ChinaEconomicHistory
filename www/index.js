2ba3f5ba22956ff76e77677d88051c25515a60ef
Fred Buchanan <fred@pineapplecomputerart.com>
2015-12-10 16:56:09 -0500

    Added Description field and place to put description.

--- www/index.js
+++ www/index.js
@@ -1,14 +1,31 @@
 var url = './data.json'
 
-var chartOptions = {'title':'Timeline of Chinies economic enteraction'}       
+var chartOptions = {'title':'Timeline of Chinies economic enteraction'}     
 
 var chart;
 var dataTable;
+var json;
 
 
 google.load("visualization","1",{packages:["timeline"]});
 
 google.setOnLoadCallback(drawChart);
+
+
+function pr(a) {
+    console.log(a);
+    return a;
+}
+
+function selectHandeler(e) {
+    selection = chart.getSelection();
+    name = dataTable.getValue(selection[0].row,1);
+    
+    discription = pr(json[name]["discription"]);
+    
+    $("#discription").text(discription);
+    
+}
 
 function drawChart() {
     
@@ -19,10 +36,14 @@ function drawChart() {
     
     dataTable.addColumn({type:'string', id:'Type' });
     dataTable.addColumn({type:'string', id:'Title' });
+    dataTable.addColumn({type:'string', role:'tooltip' });
     dataTable.addColumn({type:'date', id:'Start' });
     dataTable.addColumn({type:'date', id:'End' });
     
     populateTimelineChart();
+    
+    google.visualization.events.addListener(chart, 'select', selectHandeler);
+
     
 }
 
@@ -32,14 +53,16 @@ function populateTimelineChart() {
         
         $.each(data, function(name,value){
             
-            console.log(value);
+            //console.log(value);
+            
+            json = data;
             
             type = value["type"];
             start = value["start"];
             end = value["end"];
             
             dataTable.addRows([
-                [type,name, new Date(start,0,0), new Date(end,11,0)]
+                [type,name,'', new Date(start,0,0), new Date(end,11,0)]
             ]);
             
         });
